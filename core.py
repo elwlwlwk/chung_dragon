10ec34d1d4446a019995c804d9f40d5655bc1ca1
wisewolf <holo@wisewolf.org>
2015-09-11 07:29:46 +0000

    parse from config

--- core.py
+++ core.py
@@ -6,32 +6,45 @@ import uuid
 response=""
 corr_id=""
 correlation_id=""
+sensorList=[]
 
 config= configparser.ConfigParser()
 config.read("server.conf")
 sensors= json.loads(config['SERVER']['sensors'])
 
-def on_response(ch, method, props, body):
-	if corr_id == correlation_id:
-		response = body
+class RpcSensor(object):
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+                host='localhost'))
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
-result = channel.queue_declare(exclusive=True)
-callback_queue = result.method.queue
-channel.basic_consume(on_response, no_ack=True, queue=callback_queue)
+        self.channel = self.connection.channel()
 
-def call():
-	response = None
-	corr_id = str(uuid.uuid4())
-	channel.basic_publish(exchange='', routing_key='rpc_temperature',\
-properties=pika.BasicProperties(reply_to = callback_queue, correlation_id = corr_id,),\
-body=str(""))
-	while response is None:
-		connection.process_data_events()
-	print(response)
-	return response
+        result = self.channel.queue_declare(exclusive=True)
+        self.callback_queue = result.method.queue
 
-print(" [x] Requesting temperature")
-response = call()
-print(" [.] Got %r" % (response,))
+        self.channel.basic_consume(self.on_response, no_ack=True,
+                                   queue=self.callback_queue)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, sensor):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(exchange='',
+                                   routing_key = sensor['routing_key'],
+                                   properties = pika.BasicProperties(
+                                         reply_to = self.callback_queue,
+                                         correlation_id = self.corr_id,
+                                         ),
+                                   body=sensor['rpc_body'])
+        while self.response is None:
+            self.connection.process_data_events()
+        return (self.response)
+
+for sensor in sensors:
+	sensor_rpc = RpcSensor()
+	response = sensor_rpc.call(config[sensor])
+	sensorList.append(sensor_rpc)
+	print("Got %r" % (response))
