7fa232330e8b3221baa7ffa680044607ccf2b037
lyuha <lyuha@lyuhas-MacBook-Pro.local>
2015-10-08 16:45:38 +0900

    add basic sturucture in main.py

--- server/main.py
+++ server/main.py
@@ -1,16 +1,39 @@
 from flask import Flask, request
 from db_pool import MongoDB
-app= Flask(__name__)
+from flask_restful import Resource, Api
 
-@app.route('/')
-def root():
-    return 'root'
-	
-@app.route('/sensors', methods=['POST','GET', 'PUT', 'DELETE'])
-def sensors():
-	if request.method == 'GET':
-		MongoDB.sensor_value.insert({"test2":"test2"})
-	return request.method
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"hello": "world"}
+
+api.add_resource(HelloWorld, "/")
+
+# from 위치:string, ID:string, 센서값:string, 센서 종류:string, 단위:string
+
+SENSERS = {}
+
+class Sensors(Resource):
+    def get(self):
+        return SENSERS
+    def put(self):
+        pass
+
+api.add_resource(Sensors, "/sensors")
+
+class Sensor(Resource):
+    def get(self, sensor_id):
+        return SENSERS[sensor_id]
+    def post(self, sensor_id):
+        pass
+    def put(self, sensor_id):
+        pass
+    def delet(self, sensor_id):
+        pass
+
+api.add_resource(Sensor, "/sensors/<string:sensor_id>")
 
 if __name__=='__main__':
-	app.run(host='0.0.0.0', debug= True)
+    app.run(host='0.0.0.0', debug= True)
